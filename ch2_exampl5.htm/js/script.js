7b862a56bbdc0a0273d4a68089d7a741557e6df2
wBUDw <william.b.ward@fullsail.edu>
2014-06-10 15:31:04 -0700

    Ch2 Exmp5
    Finished example 5

---
+++ ch2_exampl5.htm/js/script.js
@@ -0,0 +1,9 @@
+var greetingString = "Hello";
+var myName = prompt("Please enter your name","");
+var concatString;
+
+document.write(greetingString + " " + myName + "<br>");
+
+concatString = greetingString + " " + myName;
+
+document.write(concatString);
