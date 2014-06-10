cb9ec115248e804d91fe455aa90a1dc02bd5e328
wBUDw <william.b.ward@fullsail.edu>
2014-06-10 15:38:24 -0700

    Ch2 Exmp6
    Finished example 6

---
+++ ch2_exampl6.htm/js/script.js
@@ -0,0 +1,7 @@
+//Equation is degCent = 5/9 (degFahren - 32).
+var degFahren = prompt("Enter the degrees in Fahrenheit",50);
+var degCent;
+
+degCent = 5/9 * (degFahren - 32);
+
+alert(degFahren + "\xb0 Fahrenheit is " + degCent + "\xb0 centigrade");
