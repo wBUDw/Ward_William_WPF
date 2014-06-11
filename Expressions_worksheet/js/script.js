6be1129385a10dfc1b32e21f33102d00fbb2da07
wBUDw <william.b.ward@fullsail.edu>
2014-06-10 21:16:15 -0700

    Activity: Expression Worksheet
    Finished dog years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,6 @@
+//Calculate how old Sparky the pit bull is in dog years
+
+var sparkysHumanAge = prompt("How old is Sparky", ""); //I created a prompt for the age of sparky
+var dogYears = 7; //this is a var to identify what human years is a multiple of
+var sparkysDogAge = ("Sparky is ") + sparkysHumanAge + (" human years old which is ") + sparkysHumanAge * dogYears + (" in dog years."); //this is the age we enter multiplied by dog years aka 7
+console.log(sparkysDogAge); //printout of Sparky's age in dog years.
