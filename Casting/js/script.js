0180a6b52028dfde18392a84f00285a27dc3a150
wBUDw <william.b.ward@fullsail.edu>
2014-06-10 20:21:32 -0700

    Activity Screencast: Expressions
    Screencast: Casting Finished

---
+++ Casting/js/script.js
@@ -0,0 +1,8 @@
+//Casting
+
+var areaCode = 909;
+var firstPart = 609;
+var secPart = 3691;
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart)
+console.log(phoneNo);
