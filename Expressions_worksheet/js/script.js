fe790cbc26f0374f6ae32a7f1bdb1bfd97856fec
wBUDw <william.b.ward@fullsail.edu>
2014-06-10 21:38:35 -0700

    Activity: Expression Worksheet
    Updated with Slice of Pie part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,18 @@
 //Calculate how old Sparky the pit bull is in dog years
 
-var sparkysHumanAge = prompt("How old is Sparky", ""); //I created a prompt for the age of sparky
+var sparkysHumanAge = prompt("How old is Sparky?", ""); //I created a prompt for the age of sparky
 var dogYears = 7; //this is a var to identify what human years is a multiple of
-var sparkysDogAge = ("Sparky is ") + sparkysHumanAge + (" human years old which is ") + sparkysHumanAge * dogYears + (" in dog years."); //this is the age we enter multiplied by dog years aka 7
-console.log(sparkysDogAge); //printout of Sparky's age in dog years.
+var sparkysDogAge = sparkysHumanAge * dogYears; //this is the age we enter multiplied by dog years aka 7
+var result1 = ("Sparky is ") + sparkysHumanAge + (" human years old which is ") + sparkysDogAge + (" in dog years.");//the result in string
+console.log(result1); //printout of Sparky's age in dog years.
+
+//calculate how much pizza each partygoer will get at the party
+
+var numberPizzas = prompt ("How many pizzas were ordered?", ""); //A prompt for the number of pizzas ordered
+var slicesPizza = prompt("How many slices of pizza are there?", ""); //A prompt for the number of slices of pizza
+var peopleAtParty = prompt("How many people attended the party?", ""); //A prompt for the number of people at the party
+var numberSliEat = numberPizzas * slicesPizza / peopleAtParty; //This is pizza multiplied by slices divided by people
+var result2 = ("Each person ate ") + numberSliEat + (" slices of pizza at the party."); // the result in string
+console.log(result2) //printout of how many slices each person ate.
+
+//
