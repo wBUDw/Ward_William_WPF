348f2172c1be6174eb7f66b8730d069e58321f2a
wBUDw <william.b.ward@fullsail.edu>
2014-06-10 22:11:09 -0700

    Activity: Expression Worksheet
    Finished Slice of Pie part II

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -9,10 +9,17 @@ console.log(result1); //printout of Sparky's age in dog years.
 //calculate how much pizza each partygoer will get at the party
 
 var numberPizzas = prompt ("How many pizzas were ordered?", ""); //A prompt for the number of pizzas ordered
-var slicesPizza = prompt("How many slices of pizza are there?", ""); //A prompt for the number of slices of pizza
+var slicesPerPizza = prompt("How many slices of pizza per pizza are there?", ""); //A prompt for the number of slices of pizza
 var peopleAtParty = prompt("How many people attended the party?", ""); //A prompt for the number of people at the party
-var numberSliEat = numberPizzas * slicesPizza / peopleAtParty; //This is pizza multiplied by slices divided by people
+var slicesOfPizza = numberPizzas * slicesPerPizza;
+var numberSliEat = slicesOfPizza / peopleAtParty; //This is slices divided by people
 var result2 = ("Each person ate ") + numberSliEat + (" slices of pizza at the party."); // the result in string
-console.log(result2) //printout of how many slices each person ate.
+console.log(result2); //printout of how many slices each person ate.
+
+//calculate how much pizza Sparky will get after the party
+
+var sparkySlices = slicesOfPizza % peopleAtParty; //This will be Sparky's slices
+var result3 = ("Sparky got ") + sparkySlices + (" slices of pizza."); //the result in string
+console.log(result3); //printout of Sparky's slices
 
 //
