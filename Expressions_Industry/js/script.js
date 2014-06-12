d2281c9c8f2b557a279fd70143e1a0c58f860975
wBUDw <william.b.ward@fullsail.edu>
2014-06-12 12:53:59 -0700

    Setting up
    I set up the var prompts for the array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,4 +4,32 @@ William Ward
 Expressions Industry
  */
 
-alert("testing");
+//alert("testing");
+
+//Making stat collector for a running back
+
+//ask for users name in a var prompt firstName
+var firstName = prompt("Hello what is your name?");
+
+//var for spaces
+var space = " ";
+
+//Introduce the running back
+var welcome = ("Welcome"+space+firstName+", I am going to ask you for your stats.");
+
+//log and alert the welcome
+console.log(welcome);
+alert(welcome);
+
+//creating var prompts rushCarQ, rushYardQ, rushTDQ, rushFumQ, recCatQ, recYardQ, recYacQ, recTDQ, to be entered in my array
+var rushCarQ = prompt(firstName+space+"how many times did you carry the football?");
+var rushYardQ = prompt(firstName+space+"how much yards did you get while carrying the football?");
+var rushTDQ = prompt(firstName+space+"how many times did you score a rushing touchdown?");
+var rushFumQ = prompt(firstName+space+"how many times did you fumble the football?");
+var recCatQ = prompt(firstName+space+"how many times did you catch the football?");
+var recYardQ = prompt(firstName+space+"how much yards did you get on catches?");
+var recYacQ = prompt(firstName+space+"how much yards after the catch did you get?");
+var recTDQ = prompt(firstName+space+"how many times did receive a catch for a touchdown?");
+
+//create array using var rushCarQ, rushYardQ, rushTDQ, rushFumQ, recCatQ, recYardQ, recYacQ, and recTDQ
+var stats =
