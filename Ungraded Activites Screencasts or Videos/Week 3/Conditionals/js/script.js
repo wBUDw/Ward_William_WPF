7baaaa91363f02f59abf00e49006325de1688d53
wBUDw <william.b.ward@fullsail.edu>
2014-06-14 12:25:23 -0700

    Finished Activity
    Finished Screencast: else if

--- Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
+++ Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
@@ -6,14 +6,17 @@ William Ward
 
 var kidHeight = 52;
 var minHeight = 48;
-
+var wParentHeight = 45; //the height of the kid with the parent
 
 //if the child is old enough, print to the console "you can ride!
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //you can ride
     console.log("You can ride the coaster!");
+}else if(kidHeight>wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
 }else{
-    //code performed if condition is false
+    //sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
