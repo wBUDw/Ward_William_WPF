36a3afd2e6fbe8c6f752c67e9c3ff5c9e6d95f2d
wBUDw <william.b.ward@fullsail.edu>
2014-06-14 12:17:01 -0700

    Finished Activity
    Finished Screencast: If and else

--- Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
+++ Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
@@ -4,14 +4,16 @@ William Ward
 
 //alert("testing");
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
 
 
 //if the child is old enough, print to the console "you can ride!
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
 }
