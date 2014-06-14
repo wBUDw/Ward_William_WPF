e6bd6cd6da138529a552f2021b6b38be449acfcb
wBUDw <william.b.ward@fullsail.edu>
2014-06-14 12:07:44 -0700

    Finished Activity
    Finished Screencast: Relational Expressions

--- Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
+++ Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
@@ -4,10 +4,12 @@ William Ward
 
 //alert("testing");
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48
 
 //if the child is old enough, print to the console "you can ride!
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
