42e17139b49987cf25e79160aeff63056bc8b9fc
wBUDw <william.b.ward@fullsail.edu>
2014-06-14 11:39:42 -0700

    Conditionals
    Finished activity Screencast: Conditionals Example

---
+++ Ungraded Activites Screencasts or Videos/Week 3/Conditionals/js/script.js
@@ -0,0 +1,13 @@
+/*
+William Ward
+ */
+
+//alert("testing");
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
