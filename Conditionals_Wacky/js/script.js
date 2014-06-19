cbe1eea2281a6f6b3ef28c082ae78543c79b0810
wBUDw <william.b.ward@fullsail.edu>
2014-06-19 14:20:02 -0700

    Setup
    Setup the index.html and the welcome on java script

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,8 +1,23 @@
 
  /*
 William Ward
-6/11/14
-Expressions Industry
+6/19/14
+Conditionals Wacky
  */
 
-alert("testing");
+//alert("testing");
+
+ //Tell user whether his World of Tanks Kills are below average, above average, or average
+
+ //welcome the user
+ var name = prompt("Please enter your name:");
+
+ //verify they did not leave it blank, if not welcome them
+ (name==="") ? name = prompt("Please tell me what your name is.") : alert("Hello "+name +".");
+
+ //log name
+ console.log(name);
+
+ //alert user what info we need
+ alert(name+" we would like to know how many kills you got in World of Tanks. We need your amount from five games.");
+
