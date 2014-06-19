f5e04c693831f14f5e9962f80dbf7da43cc07564
wBUDw <william.b.ward@fullsail.edu>
2014-06-19 13:14:27 -0700

    Finished
    Completed code, cleaned up, and fixed issues.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -29,71 +29,71 @@ Conditionals Personal
  var hoursGamePlay = [prompt(name+" how many hours did you play Sunday?"), prompt(name+" how many hours did you play Monday?"), prompt(name+" how many hours did you play Tuesday?"), prompt(name+" how many hours did you play Wednesday?"), prompt(name+" how many hours did you play Thursday?"), prompt(name+" how many hours did you play Friday?"), prompt(name+" how many hours did you play Saturday?")];
 
 
- //verify hoursGamePlay[0] is a number || 24hrs or less
-if (isNaN(hoursGamePlay[0]) || hoursGamePlay[0]>=hoursInDay){
+ //verify hoursGamePlay[0] is a number, 24hrs or less, or not blank
+if (isNaN(hoursGamePlay[0]) || hoursGamePlay[0]>=hoursInDay || hoursGamePlay[0]===""){
 
     //prompt again for the hours
-    hoursGamePlay[0] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Sunday?");
+    hoursGamePlay[0] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Sunday?");
 }
 
  //log hoursGamePlay[0]
  console.log(hoursGamePlay[0]);
 
- //verify hoursGamePlay[1] is a number || 24hrs or less
- if (isNaN(hoursGamePlay[1]) || hoursGamePlay[1]>=hoursInDay){
+ //verify hoursGamePlay[1] is a number, 24hrs or less, or not blank
+ if (isNaN(hoursGamePlay[1]) || hoursGamePlay[1]>=hoursInDay || hoursGamePlay[1]===""){
 
      //prompt again for the hours
-     hoursGamePlay[1] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Monday?");
+     hoursGamePlay[1] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Monday?");
  }
 
  //log hoursGamePlay[1]
  console.log(hoursGamePlay[1]);
 
- //verify hoursGamePlay[2] is a number || 24hrs or less
- if (isNaN(hoursGamePlay[2]) || hoursGamePlay[2]>=hoursInDay){
+ //verify hoursGamePlay[2] is a number, 24hrs or less, or not blank
+ if (isNaN(hoursGamePlay[2]) || hoursGamePlay[2]>=hoursInDay || hoursGamePlay[2]===""){
 
      //prompt again for the hours
-     hoursGamePlay[2] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Tuesday?");
+     hoursGamePlay[2] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Tuesday?");
  }
 
  //log hoursGamePlay[2]
  console.log(hoursGamePlay[2]);
 
- //verify hoursGamePlay[3] is a number || 24hrs or less
- if (isNaN(hoursGamePlay[3]) || hoursGamePlay[3]>=hoursInDay){
+ //verify hoursGamePlay[3] is a number, 24hrs or less, or not blank
+ if (isNaN(hoursGamePlay[3]) || hoursGamePlay[3]>=hoursInDay || hoursGamePlay[3]===""){
 
      //prompt again for the hours
-     hoursGamePlay[3] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Wednesday?");
+     hoursGamePlay[3] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Wednesday?");
  }
 
  //log hoursGamePlay[3]
  console.log(hoursGamePlay[3]);
 
- //verify hoursGamePlay[4] is a number || 24hrs or less
- if (isNaN(hoursGamePlay[4]) || hoursGamePlay[4]>=hoursInDay){
+ //verify hoursGamePlay[4] is a number, 24hrs or less, or not blank
+ if (isNaN(hoursGamePlay[4]) || hoursGamePlay[4]>=hoursInDay || hoursGamePlay[4]===""){
 
      //prompt again for the hours
-     hoursGamePlay[4] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Thursday?");
+     hoursGamePlay[4] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Thursday?");
  }
 
  //log hoursGamePlay[4]
  console.log(hoursGamePlay[4]);
 
- //verify hoursGamePlay[5] is a number || 24hrs or less
- if (isNaN(hoursGamePlay[5]) || hoursGamePlay[5]>=hoursInDay){
+ //verify hoursGamePlay[5] is a number, 24hrs or less, or not blank
+ if (isNaN(hoursGamePlay[5]) || hoursGamePlay[5]>=hoursInDay || hoursGamePlay[5]===""){
 
      //prompt again for the hours
-     hoursGamePlay[5] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Friday?");
+     hoursGamePlay[5] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Friday?");
  }
 
  //log hoursGamePlay[5]
  console.log(hoursGamePlay[5]);
 
- //verify hoursGamePlay[6] is a number || 24hrs or less
- if (isNaN(hoursGamePlay[6]) || hoursGamePlay[6]>=hoursInDay){
+ //verify hoursGamePlay[6] is a number, 24hrs or less, or not blank
+ if (isNaN(hoursGamePlay[6]) || hoursGamePlay[6]>=hoursInDay || hoursGamePlay[6]===""){
 
      //prompt again for the hours
-     hoursGamePlay[6] = prompt(name+" the number you gave was either not a number or more than 24 hours. How many hours did you play Saturday?");
+     hoursGamePlay[6] = prompt(name+" the number you gave was either not a number, you left it blank, or more than 24 hours. How many hours did you play Saturday?");
  }
 
  //log hoursGamePlay[6]
@@ -107,6 +107,9 @@ if (isNaN(hoursGamePlay[0]) || hoursGamePlay[0]>=hoursInDay){
 
  //calculate the average of the array
  var averageHours = totalHours/7;
+
+ //have the averageHours rounded to the nearest hundredth
+ averageHours = averageHours.toFixed(2);
 
  //log average of array
  console.log(averageHours);
@@ -127,7 +130,7 @@ if (isNaN(hoursGamePlay[0]) || hoursGamePlay[0]>=hoursInDay){
  } else if (totalHours<16) {
 
      //var for the result2 if averageHours is less than 16
-     var result2 = name+" congrats on keeping your total hours per week reasonable. You play "+totalHours+" hours per-week, and average"+averageHours+" hours per day, playing video games.";
+     var result2 = name+" congrats on keeping your total hours per week reasonable. You play "+totalHours+" hours per-week, and average "+averageHours+" hours per day, playing video games.";
 
      //log result2
      console.log(result2);
@@ -138,7 +141,7 @@ if (isNaN(hoursGamePlay[0]) || hoursGamePlay[0]>=hoursInDay){
  } else if (totalHours<30) {
 
      //var for the result3 if averageHours less than 30
-     var result3 = name+" your total hours are in the ball park of a part-time job. You play "+totalHours+" hours per-week, and average"+averageHours+" hours per day, playing video games.";
+     var result3 = name+" your total hours are in the ball park of a part-time job. You play "+totalHours+" hours per-week, and average "+averageHours+" hours per day, playing video games.";
 
      //log result3
      console.log(result3);
@@ -149,7 +152,7 @@ if (isNaN(hoursGamePlay[0]) || hoursGamePlay[0]>=hoursInDay){
  } else{
 
      //var for the result4 if averageHours is greater than 29
-     var result4 = name+" your total hours are best compared to a full-time job. You play "+totalHours+" hours per-week, and average"+averageHours+" hours per day, playing video games.";
+     var result4 = name+" your total hours are best compared to a full-time job. You play "+totalHours+" hours per-week, and average "+averageHours+" hours per day, playing video games.";
 
      //log result4
      console.log(result4);
