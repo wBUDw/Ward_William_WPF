3eb0af58992019e3ec635d891ad4089b22467ead
wBUDw <william.b.ward@fullsail.edu>
2014-06-25 21:20:35 -0700

    More Work
    Validated mph, function for sum and average of hours

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -95,7 +95,7 @@ alert("testing");
  //validate tredHours[1] with while loop
  while (tredHours[1]==="" || isNaN(tredHours[1])){
 
-     //Re-prompt for tredHours[0]
+     //Re-prompt for tredHours[1]
      tredHours[1] = prompt("Please do not leave blank, and use only numbers. Day two, how many hours did you walk?")
 
  }
@@ -103,23 +103,73 @@ alert("testing");
  //validate tredHours[2] with while loop
  while (tredHours[2]==="" || isNaN(tredHours[2])){
 
-     //Re-prompt for tredHours[0]
+     //Re-prompt for tredHours[2]
      tredHours[2] = prompt("Please do not leave blank, and use only numbers. Day three, how many hours did you walk?")
 
  }
 
- //log array length
+ //log array tredHours
  console.log(tredHours);
 
  //create array for speed in mph walked
  var tredMph = [prompt("Day one, what mph setting did you use?"), prompt("Day two, what mph setting did you use?"), prompt("Day three, what mph setting did you use?")];
 
- //log array length
+ //validate tredMph[0] with while loop
+ while (tredMph[0]==="" || isNaN(tredMph[0])){
+
+     //Re-prompt for tredMph[0]
+     tredMph[0] = prompt("Please do not leave blank, and use only numbers. Day one, what mph setting did you use?")
+
+ }
+
+ //validate tredMph[1] with while loop
+ while (tredMph[1]==="" || isNaN(tredMph[1])){
+
+     //Re-prompt for tredMph[1]
+     tredMph[1] = prompt("Please do not leave blank, and use only numbers. Day two, what mph setting did you use?")
+
+ }
+
+ //validate tredMph[2] with while loop
+ while (tredMph[2]==="" || isNaN(tredMph[2])){
+
+     //Re-prompt for tredMph[2]
+     tredMph[2] = prompt("Please do not leave blank, and use only numbers. Day three, what mph setting did you use?")
+
+ }
+
+ //log array tredMph
  console.log(tredMph);
 
+ //anonymous function for totalHours
+ var totalHours =function(day1SH,day2SH,day3SH){
 
+     //calculate totalHours
+     var sumHours = Number(day1SH)+Number(day2SH)+Number(day3SH);
+     //return sumHours
+     return sumHours;
+ };
 
+ //create var for
+ var tredSumHours = totalHours(tredHours[0],tredHours[1],tredHours[2]);
 
+ //log tredSumHours
+ console.log(tredSumHours);
 
+ //anonymous function for averageHours
+ var averageHours =function(day1SH,day2SH,day3SH){
 
+     //calculate averageHours
+     var aveHours = (Number(day1SH)+Number(day2SH)+Number(day3SH))/tredHours.length;
+     //return sumHours
+     return aveHours;
+ };
 
+ //create var for
+ var tredAveHours = averageHours(tredHours[0],tredHours[1],tredHours[2]);
+
+ //round tredAveHours to nearest hundredth
+ var roundTredAveHours = tredAveHours.toFixed(2);
+
+ //log tredSumHours
+ console.log(roundTredAveHours);
